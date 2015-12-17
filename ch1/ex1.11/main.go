17d048827f8958c0967ddba91ff198cc4130a437
Mitsuteru Sawa <mitsuteru.s@gmail.com>
2015-12-17 20:27:12 +0900

    Add ex1.11

---
+++ ch1/ex1.11/main.go
@@ -0,0 +1,6 @@
+// Try fetchall with longer argument lists, such as samples from the top million
+// web sites available at alexa.com. How does the program behave if a web site
+// just doesn't respond?
+
+// Return the same format if it success...
+package main
